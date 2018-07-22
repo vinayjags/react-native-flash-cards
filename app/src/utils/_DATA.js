7caf433ec0caf62a2fa36b9a530e8c0513072b38
Vinay Jagnani <vinay.jagnani@gmail.com>
2018-07-22 16:07:21 +0530

    feat: work on redux setup

---
+++ app/src/utils/_DATA.js
@@ -0,0 +1,20 @@
+let users = {
+  sarahedo: {
+    id: "sarahedo",
+    name: "Sarah Edo",
+    avatarURL: "six.png",
+    flashCards: {}
+  },
+  tylermcginnis: {
+    id: "tylermcginnis",
+    name: "Tyler McGinnis",
+    avatarURL: "three.png",
+    flashCards: {}
+  },
+  johndoe: {
+    id: "johndoe",
+    name: "John Doe",
+    avatarURL: "two.png",
+    flashCards: {}
+  }
+};
